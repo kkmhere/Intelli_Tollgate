ce37bef4d2140bc3d3b20ee13865fa74368275d0
kkmhere <kkm01.1998@gmail.com>
2019-07-01 13:37:26 +0530

    admin & user login activity added

---
+++ app/src/main/java/com/example/intelli_tollgate/admin_login.java
@@ -0,0 +1,14 @@
+package com.example.intelli_tollgate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class admin_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+    }
+}
