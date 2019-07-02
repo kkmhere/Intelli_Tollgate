31eb98e1e5f33a4b48d5d73b1b518d836f07955c
kkmhere <kkm01.1998@gmail.com>
2019-07-02 18:35:15 +0530

    Revert "register added"
    This reverts commit 7c622674687dd2533f8b95ceebb20f0308c7bab0.
    
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/intelli_tollgate/home.java
    #	app/src/main/java/com/example/intelli_tollgate/register.java
    #	app/src/main/java/com/example/intelli_tollgate/user_login.java
    #	app/src/main/res/layout/activity_home.xml
    #	app/src/main/res/layout/activity_register.xml
    #	app/src/main/res/layout/activity_user_login.xml

--- app/src/main/java/com/example/intelli_tollgate/home.java
+++ app/src/main/java/com/example/intelli_tollgate/home.java
@@ -1,86 +1,14 @@
 package com.example.intelli_tollgate;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class home extends AppCompatActivity {
-    Button pay1,pay2,pay3,pay4,pay5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        pay1 = findViewById(R.id.pay1);
-        pay2 = findViewById(R.id.pay2);
-        pay3 = findViewById(R.id.pay3);
-        pay4 = findViewById(R.id.pay4);
-        pay5 = findViewById(R.id.pay5);
-
-        pay1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        pay2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        pay3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        pay4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        pay5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        Toolbar toolbar=(Toolbar)findViewById(R.id.mytoolbar);
-        if(toolbar!=null)
-        {
-            setSupportActionBar(toolbar);
-        }
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mymenu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        switch(item.getItemId())
-        {
-            case R.id.wallet:
-                Toast.makeText(home.this,"WALLET SELECTED",Toast.LENGTH_SHORT).show();
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
