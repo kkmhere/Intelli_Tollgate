12f47b4232996965c41e1d551a72fb7593a087f2
kkmhere <kkm01.1998@gmail.com>
2019-07-02 18:38:29 +0530

    Revert "wallet icon added"
    This reverts commit 1e687c25

--- app/src/main/java/com/example/intelli_tollgate/home.java
+++ app/src/main/java/com/example/intelli_tollgate/home.java
@@ -1,13 +1,8 @@
 package com.example.intelli_tollgate;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 public class home extends AppCompatActivity {
 
@@ -15,29 +10,5 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        Toolbar toolbar=(Toolbar)findViewById(R.id.mytoolbar);
-        if(toolbar!=null)
-        {
-            setSupportActionBar(toolbar);
-        }
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mymenu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        switch(item.getItemId())
-        {
-            case R.id.wallet:
-                Toast.makeText(home.this,"WALLET SELECTED",Toast.LENGTH_SHORT).show();
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
