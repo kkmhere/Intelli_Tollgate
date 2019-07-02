1592514b4ac67b502fa936995e27f25cc329ffd6
kvk123 <kv58265@gmail.com>
2019-07-02 13:27:41 +0530

    Home_interface_protocol_

--- app/src/main/java/com/example/intelli_tollgate/home.java
+++ app/src/main/java/com/example/intelli_tollgate/home.java
@@ -3,18 +3,61 @@ package com.example.intelli_tollgate;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class home extends AppCompatActivity {
+    Button pay1,pay2,pay3,pay4,pay5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        pay1 = findViewById(R.id.pay1);
+        pay2 = findViewById(R.id.pay2);
+        pay3 = findViewById(R.id.pay3);
+        pay4 = findViewById(R.id.pay4);
+        pay5 = findViewById(R.id.pay5);
+
+        pay1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        pay2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        pay3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        pay4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        pay5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         Toolbar toolbar=(Toolbar)findViewById(R.id.mytoolbar);
         if(toolbar!=null)
