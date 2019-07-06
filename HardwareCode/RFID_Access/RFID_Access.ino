3b10802655094d78bc6680dedfdb6d857ecb71c3
rajj6 <krishnkant100.99@gmail.com>
2019-07-06 11:42:55 +0530

    Traffic Lights Added

--- HardwareCode/RFID_Access/RFID_Access.ino
+++ HardwareCode/RFID_Access/RFID_Access.ino
@@ -5,6 +5,9 @@
 #define SS_PIN 4  //D2
 #define RST_PIN 5 //D1
 
+#define RED_LED 16  // D0
+#define GREEN_LED 0 // D3
+
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Servo.h>
@@ -50,6 +53,11 @@ void setup_wifi()
 void setup() 
 {
   servo.attach(2);//D4
+  pinMode(GREEN_LED,OUTPUT);   //TX Grenn LED
+  pinMode(RED_LED,OUTPUT);   // Red Light
+
+  digitalWrite(GREEN_LED,LOW);  //Green OFF
+  digitalWrite(RED_LED,HIGH);  //Red ON
   servo.write(90);  // gate closed
   delay(1000);
   Serial.begin(9600);   // Initiate a serial communication
@@ -144,7 +152,11 @@ void loop()
     client.publish("outTopic007", msg);
     Serial.println("You can go!");
     servo.write(0); //Gate open
+    digitalWrite(GREEN_LED,HIGH); // Green Light ON //Tx
+    digitalWrite(RED_LED,LOW);  // Red OFF    //Rx
     delay(10000);
+    digitalWrite(GREEN_LED,LOW);  //Green OFF
+    digitalWrite(RED_LED,HIGH);   //Red ON
     servo.write(90);  //gate Closed
     Serial.println("Next Please!");
     //Serial.println();
@@ -153,6 +165,8 @@ void loop()
   else   
   {
     Serial.println(" Access Denied ");
+    digitalWrite(GREEN_LED,LOW);  //Green OFF
+    digitalWrite(RED_LED,HIGH);  //Red ON
     msg = "Access Denied!";
     client.publish("outTopic007", msg);
     delay(3000);
