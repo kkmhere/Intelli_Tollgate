c48b7754bef8dfbc8390eef28b97ee0a4ddae893
kkmhere <kkm01.1998@gmail.com>
2019-07-08 21:51:16 +0530

    changes

--- HardwareCode/LCD_test/LCD_test.ino
+++ HardwareCode/LCD_test/LCD_test.ino
@@ -1,7 +1,8 @@
 #include <LiquidCrystal.h>
 const int RS = 6,EN = 7, d4 = 11, d5 = 8, d6 = 9, d7 = 10;   
 LiquidCrystal lcd(RS, EN, d4, d5, d6, d7);
-void setup() {
+void setup() 
+{
   // put your setup code here, to run once:
    lcd.begin(16, 2);
                       // Print a message to the LCD.
@@ -9,7 +10,8 @@ void setup() {
 
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
